e9aa21a50b7d23123eeedc10be6e6c1ca6d3e986
beardedguru <charles@thebeardedguru.com>
2017-12-29 16:38:58 -0500

    Added new Projects View

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-})
+//  Maintenance Page
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -46,6 +48,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+})
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
   });
 })
 
